1d2186243e01066246e5812ad5645d484bfe1dd5
michelle2005Romero <142345226+michelle2005Romero@users.noreply.github.com>
2023-09-19 11:26:27 -0500

    RegistroProductos.java
    En el siguiente código se realizo el registro de los productos, donde se le solicita al usuario el numero de productos a registrar, junto con el nombre, precio y código del producto correspondiente

--- RegistroProductos.java
+++ RegistroProductos.java
@@ -1,17 +1,102 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class RegistroProductos {
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Producto {
+    private final String nombre;
+    private final double precio;
+    private final int codigo;
+
+    public Producto(String nombre, double precio, int codigo) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto: " + nombre + "\nPrecio: $" + precio + "\nCodigo: " + codigo;
+    }
+}
+
+public class CajaRegistradora {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Producto> productos = new ArrayList<>();
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.print("Ingrese la cantidad de productos: ");
+        int cantidadProductos = obtenerEnteroPositivo(scanner);
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (int i = 0; i < cantidadProductos; i++) {
+            scanner.nextLine(); // Consumir la nueva línea pendiente
+
+            // Solicitar los detalles de cada producto al usuario
+            System.out.println("Producto #" + (i + 1));
+            System.out.print("Nombre del producto: ");
+            String nombre = scanner.nextLine();
+            double precio = obtenerDoublePositivo(scanner);
+            int codigo = obtenerEnteroPositivo(scanner);
+
+            // Crear un objeto Producto y agregarlo a la lista
+            productos.add(new Producto(nombre, precio, codigo));
+        }
+
+        double total = 0;
+        System.out.println("\nRecibo de compra:");
+
+        // Mostrar los detalles de cada producto en el recibo
+        for (Producto producto : productos) {
+            System.out.println(producto + "\n");
+            total += producto.getPrecio();
+        }
+
+        // Mostrar el total de la compra
+        System.out.println("Total de la compra: $" + total);
+    }
+
+    // Función para obtener un entero positivo desde la entrada del usuario
+    private static int obtenerEnteroPositivo(Scanner scanner) {
+        while (true) {
+            try {
+                int numero = scanner.nextInt();
+                if (numero > 0) {
+                    return numero;
+                } else {
+                    System.out.println("Por favor, ingrese un número entero positivo.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese un número entero positivo válido.");
+                scanner.nextLine(); // Consumir la entrada inválida
+            }
+        }
+    }
+
+    // Función para obtener un número decimal positivo desde la entrada del usuario
+    private static double obtenerDoublePositivo(Scanner scanner) {
+        while (true) {
+            try {
+                double numero = scanner.nextDouble();
+                if (numero > 0) {
+                    return numero;
+                } else {
+                    System.out.println("Por favor, ingrese un número decimal positivo.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese un número decimal positivo válido.");
+                scanner.nextLine(); // Consumir la entrada inválida
+            }
         }
     }
 }
